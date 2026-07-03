93c577690105d40449f213cc2a71e4988e9465a4
xjy0920 <1330253973@qq.com>
2026-07-03 20:32:09 +0800

    Add files via upload
    add

---
+++ AFLtest.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <string.h>
+int calculate(int a, int b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case 'x': return a * b;
+        case '/': return b ? a / b : 0;
+        default: return 0;
+    }}
+int main() {
+    char input[100];
+
+    gets(input);
+
+    int a, b, result;
+    char op;
+
+    if (sscanf(input, "%d %c %d", &a, &op, &b) != 3)
+        return 1;
+
+    result = calculate(a, b, op);
+    printf("Result: %d\n", result);
+
+    return 0;}
